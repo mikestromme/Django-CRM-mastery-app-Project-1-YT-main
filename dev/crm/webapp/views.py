d5b49668ab862dd8cd4481260162bac42dacc044
Mike Stromme <mikestromme@gmail.com>
2024-04-25 05:32:55 -0500

    x

--- dev/crm/webapp/views.py
+++ dev/crm/webapp/views.py
@@ -1,70 +1,40 @@
 from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm, CreateRecordForm, UpdateRecordForm
-
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
-
 from django.contrib.auth.decorators import login_required
-
 from .models import Record
-
 from django.contrib import messages
 
-
-
 # - Homepage 
-
 def home(request):
-
     return render(request, 'webapp/index.html')
 
-
 # - Register a user
-
 def register(request):
-
     form = CreateUserForm()
-
     if request.method == "POST":
-
         form = CreateUserForm(request.POST)
-
         if form.is_valid():
-
             form.save()
-
             messages.success(request, "Account created successfully!")
-
             return redirect("my-login")
-
     context = {'form':form}
-
     return render(request, 'webapp/register.html', context=context)
 
 
 # - Login a user
-
 def my_login(request):
-
     form = LoginForm()
-
     if request.method == "POST":
-
         form = LoginForm(request, data=request.POST)
-
         if form.is_valid():
-
             username = request.POST.get('username')
             password = request.POST.get('password')
-
             user = authenticate(request, username=username, password=password)
-
             if user is not None:
-
                 auth.login(request, user)
-
                 return redirect("dashboard")
-
     context = {'form':form}
 
     return render(request, 'webapp/my-login.html', context=context)
